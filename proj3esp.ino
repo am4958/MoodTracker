5df71c1db008d98a04eaacb2ae83431897bffa21
am4958 <46966950+am4958@users.noreply.github.com>
2021-03-02 18:15:14 -0500

    Create proj3esp.ino

---
+++ proj3esp.ino
@@ -0,0 +1,72 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <Keypad.h>
+//set up to connect to an existing network (e.g. mobile hotspot from laptop that will run the python code)
+const char* ssid = "APlaceToDequeue";
+const char* password = "elsebeer++";
+WiFiUDP Udp;
+unsigned int localUdpPort = 4210;  //  port to listen on
+char incomingPacket[255];  // buffer for incoming packets
+
+char keys[4][4] = {
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+byte rowPins[4] = {14, 27, 26, 25}; // connect to the row pinouts of the keypad
+byte colPins[4] = {13, 21, 22, 23}; // connect to the column pinouts of the keypad
+// initialize an instance of class NewKeypad
+Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
+
+void setup()
+{
+  int status = WL_IDLE_STATUS;
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to wifi");
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+
+  // we recv one packet from the remote so we can know its IP and port
+  bool readPacket = false;
+  while (!readPacket) {
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
+     {
+      // receive incoming UDP packets
+      Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+      int len = Udp.read(incomingPacket, 255);
+      if (len > 0)
+      {
+        incomingPacket[len] = 0;
+      }
+      Serial.printf("UDP packet contents: %s\n", incomingPacket);
+      readPacket = true;
+    } 
+  }
+}
+
+void loop()
+{
+  
+  // once we know where we got the inital packet from, send data back to that IP address and port
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  
+ char keyPressed = myKeypad.getKey();
+   if (keyPressed) {
+    Udp.print(keyPressed);
+  }
+  // Just test touch pin - Touch0 is T0 which is on GPIO 4.
+//  Udp.printf(String(touchRead(T0)).c_str(),2);
+  Udp.endPacket();
+  delay(1000);
+}
